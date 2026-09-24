0e0efbda21f25d34f6b39e89b53d4def5990a5c7
Shobahan758 <shobahan758@gmail.com>
2026-09-24 17:17:19 +0600

    Use solutionmart.com as storefront domain

--- tests/Feature/LandingSettingsTest.php
+++ tests/Feature/LandingSettingsTest.php
@@ -111,7 +111,7 @@ class LandingSettingsTest extends TestCase
     {
         $html = $this->get(route('home'))
             ->assertOk()
-            ->assertSee('<link rel="canonical" href="https://furniturepolish.solutionmart.net/">', false)
+            ->assertSee('<link rel="canonical" href="https://furniturepolish.solutionmart.com/">', false)
             ->assertSee('"@type":"Product"', false)
             ->assertSee('"priceCurrency":"BDT"', false)
             ->assertDontSee('"aggregateRating"', false)
