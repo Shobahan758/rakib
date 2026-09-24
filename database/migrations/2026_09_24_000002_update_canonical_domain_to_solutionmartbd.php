6c93869031e94825bd89d0768304bfe83e8753de
Shobahan758 <shobahan758@gmail.com>
2026-09-24 18:05:28 +0600

    Use solutionmartbd.com as storefront domain

---
+++ database/migrations/2026_09_24_000002_update_canonical_domain_to_solutionmartbd.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        if (! Schema::hasTable('landing_sections')) {
+            return;
+        }
+
+        $section = DB::table('landing_sections')->where('slug', 'seo')->first();
+        if (! $section) {
+            return;
+        }
+
+        $content = json_decode($section->content, true);
+        if (! is_array($content)) {
+            return;
+        }
+
+        $previousDomains = [
+            'https://ss.smarteasyshop.com/',
+            'https://furniturepolish.solutionmart.net/',
+            'https://furniturepolish.solutionmart.com/',
+        ];
+
+        if (! in_array($content['canonical_url'] ?? null, $previousDomains, true)) {
+            return;
+        }
+
+        $content['canonical_url'] = 'https://furniturepolish.solutionmartbd.com/';
+
+        DB::table('landing_sections')->where('id', $section->id)->update([
+            'content' => json_encode($content, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES),
+            'updated_at' => now(),
+        ]);
+    }
+
+    public function down(): void
+    {
+        // Production URLs are intentionally not reverted.
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,7 @@ Route::get('/', function () {
 Route::get('/sitemap.xml', function () {
     $xml = '<?xml version="1.0" encoding="UTF-8"?>'
         .'<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
-        .'<url><loc>https://furniturepolish.solutionmart.com/</loc><changefreq>weekly</changefreq><priority>1.0</priority></url>'
+        .'<url><loc>https://furniturepolish.solutionmartbd.com/</loc><changefreq>weekly</changefreq><priority>1.0</priority></url>'
         .'</urlset>';
 
     return response($xml, 200)->header('Content-Type', 'application/xml; charset=UTF-8');
